f3907a30b2bc248c025f1bfa45547afcc4467a4d
FengYiLiao <86756536+FengYiLiao@users.noreply.github.com>
2022-04-12 20:58:49 -0700

    Delete TestSDPdata.m

--- TestSDPdata.m
+++
@@ -1,7 +0,0 @@
-%TestSDPdata
-
-clc;clear; 
-dx = 1;
-load('SedumiData\n10m10.mat');
-At_sdp = full(At_sdp); b_sdp = full(b_sdp); c_sdp = full(c_sdp);
-Objs =  InnerApproximation(At_sdp,b_sdp,c_sdp,K_sdp,dx);
