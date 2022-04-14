8b3c8605cd809fd3710f654d1a133fa6f1808e91
FengYiLiao <86756536+FengYiLiao@users.noreply.github.com>
2022-04-14 11:29:21 -0700

    Update TestSDPdata.m

--- TestSDPdata.m
+++ TestSDPdata.m
@@ -1,8 +1,9 @@
 %TestSDPdata
 
 clc;clear; 
+addpath('Module\');
 dx = 1;
 Maxiter = 10;
-load('SedumiData\n1500m10.mat');
+load('SedumiData\n10m10.mat');
 At_sdp = full(At_sdp); b_sdp = full(b_sdp); c_sdp = full(c_sdp);
 Objs =  InnerApproximation(At_sdp,b_sdp,c_sdp,K_sdp,dx,Maxiter);
